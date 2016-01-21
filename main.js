6f30059c5db23dc0c2e85a9a3c914d8e6b64d902
Valentin Miculit <valsbox@gmail.com>
2016-01-21 14:49:28 -0700

    completed

--- main.js
+++ main.js
@@ -2,6 +2,43 @@
 angular.module("js5",[])
 
 angular.module("js5")
-	.controller("js5Troller",["$scope", function($scope) {
+	.controller("js5Troller",["$scope","$timeout", function($scope, $timeout) {
+
+		$scope.user = {
+			name : "John Doe",
+			email : "john@someplace.com",
+			password : "secret"
+		}
+
+		$scope.inputSwitch = [
+			false,
+			false,
+			false
+		]
+
+		$scope.textboxSwitch = [
+			true,
+			true,
+			true
+		]
+
+		$scope.swapFieldsOn = function(n, event) {
+
+			$scope.inputSwitch[n] = !$scope.inputSwitch[n]
+			$scope.textboxSwitch[n] = !$scope.textboxSwitch[n]
+
+			$timeout(function() {
+				event.target.nextElementSibling.focus()
+			})
+
+		}
+
+		$scope.swapFieldsOff = function(n, event) {
+
+			$scope.inputSwitch[n] = !$scope.inputSwitch[n]
+			$scope.textboxSwitch[n] = !$scope.textboxSwitch[n]
+
+		}
+
 
 }])
